a2301ca1f707ac832b6a16770de1081254487a88
dptorri <dptorri@gmail.com>
2021-07-01 13:32:29 +0200

    7.2 Create a UserDAOService

---
+++ springboot/src/main/java/springboot/rest/user/UserDaoService.java
@@ -0,0 +1,62 @@
+package springboot.rest.user;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+
+    private static int usersCount = 3;
+
+    // Add a static list
+    static {
+        users.add(new User(1, "Adam", new Date()));
+        users.add(new User(2, "Eve", new Date()));
+        users.add(new User(3, "Jack", new Date()));
+    }
+
+    /**
+     * findAll: Retrieve all users
+     * @return
+     */
+    public List<User> findAll() {
+        return users;
+    }
+
+    /**
+     * save: Create new user
+     * @param user
+     * @return
+     */
+    public User save(User user) {
+        // if not created acumulate id
+        if(user.getId()==null) {
+            user.setId(++usersCount);
+        }
+
+        // save user
+        users.add(user);
+
+        //return user
+        return user;
+    }
+
+    /**
+     * findOne: Retrieve user by id
+     * @param userId
+     * @return
+     */
+    public User findOne(Integer userId){
+        for(User user: users) {
+            if(user.getId() == userId){
+                return user;
+            }
+        }
+        return null;
+    }
+
+}
