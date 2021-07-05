e7c786033bbedd2266238ffea0e385f0628318ce
dptorri <dptorri@gmail.com>
2021-07-05 14:52:28 +0200

    7.4 Create a UserResource to input details of a user

--- springboot/src/main/java/springboot/rest/user/UserResource.java
+++ springboot/src/main/java/springboot/rest/user/UserResource.java
@@ -1,9 +1,7 @@
 package springboot.rest.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,11 @@ public class UserResource {
     @GetMapping("/user/{id}")
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
     }
 
 }
